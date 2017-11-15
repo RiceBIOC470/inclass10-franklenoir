cdf57aa06d7a607889304f1349d68856e0133a86
gbri140 <glb3@rice.edu>
2017-11-14 20:49:40 -0600

    Update inclass10.m

--- inclass10.m
+++ inclass10.m
@@ -1,6 +1,13 @@
 %In this directory, you will find some example data, which is a 300x8
 %array stored in a .mat file. Image these are measurements for 300 genes are 8 different time
 %points
+%GB comment;
+1)100
+2)100
+3)100
+4)100
+overall: 100
+
 
 %Walter Frank Lenoir
 
